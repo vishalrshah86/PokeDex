7e54cc669e55ea8e0d817808499026687f7501f4
vishalrshah <vishalrshah86@gmail.com>
2021-06-13 17:18:41 -0400

    New project codes rearranged and corrected

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,5 @@
 let pokemonList = [
- {
-  name: "Clefable",
-  height: 3,
-  types: ['grass','speed'],
- };
-
- {
-  name: 'Pikachu',
-  height: 4,
-  types: ['water','speed'],
- };
-
- {
-  name: 'Sandshrew',
-  height: 5,
-  types: ['ice','speed'],
- };
+ {name: "Clefable", height: 3, types: ['grass','speed']},
+ {name: 'Pikachu', height: 4, types: ['water','speed']},
+ {name: 'Sandshrew', height: 5, types: ['ice','speed']}
 ];
