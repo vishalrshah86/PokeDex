9d2a576c92561c06f7b60c2ab2b26b4377678fdb
vishalrshah <vishalrshah86@gmail.com>
2021-06-12 21:11:27 -0400

    Update scripts.js
    Three Pokémon objects created

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+let pokemonList = [
+ {
+  name: "Clefable",
+  height: 3,
+  types: ['grass','speed'],
+ };
+
+ {
+  name: 'Pikachu',
+  height: 4,
+  types: ['water','speed'],
+ };
+
+ {
+  name: 'Sandshrew',
+  height: 5,
+  types: ['ice','speed'],
+ };
+];
