cf44ac7aada465a1d5b632185b61e61dd12bb5e5
vishalrshah <vishalrshah86@gmail.com>
2021-06-14 11:39:44 -0400

    Created for loop  variable to each pokemon   and added conditionals
    Added for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,19 @@
 let pokemonList = [
- {name: "Clefable", height: 3, types: ['grass','speed']},
- {name: 'Pikachu', height: 4, types: ['water','speed']},
- {name: 'Sandshrew', height: 5, types: ['ice','speed']}
+ {name: "Clefable", height: 1, types: ['grass','speed']},
+ {name: 'Pikachu', height: 1.5, types: ['water','speed']},
+ {name: 'Sandshrew', height: 2.3, types: ['ice','speed']}
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+    // write the Pokémon name on your website’s DOM.
+  document.write(pokemonList[i].name +  "(height:" + pokemonList[i].height + ")");
+
+  //Checking if pokemon height is greater than 1.5 m
+if (pokemonList[i].height > 1.5) {
+ //if pokemon height is greater than 1.6 m, writes "Wow that's big!"
+  document.write(" - Wow, that's big!");
+  }
+
+ //Adding linebreaks after each pokemon name
+  document.write("<br><br>");
+  }
