3d578addd4ec614ffe4143340ba0c1d63adceb8f
vishalrshah <vishalrshah86@gmail.com>
2021-10-28 18:53:05 -0400

    Corrections and indentation

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,28 @@
 let pokemonRepository = (function() {
 //added IIFE function
-let pokemonList = [
- {name: "Clefable", height: 1, types: ['grass','speed']},
- {name: 'Pikachu', height: 1.5, types: ['water','speed']},
- {name: 'Sandshrew', height: 2.3, types: ['ice','speed']},
- {name: 'Bulbasaur', height: 0.7, types: ['grass','poison']},
- {name: 'Charizard', height: 1.7, types: ['fire','flying']}
-];
+    let pokemonList = [
+      {name: "Clefable", height: 1, types: ['grass','speed']},
+      {name: 'Pikachu', height: 1.5, types: ['water','speed']},
+      {name: 'Sandshrew', height: 2.3, types: ['ice','speed']},
+      {name: 'Bulbasaur', height: 0.7, types: ['grass','poison']},
+      {name: 'Charizard', height: 1.7, types: ['fire','flying']}
+    ];
 
-function getAll() {
-   return pokemonList;
-}
+    function getAll() {
+      return pokemonList;
+    }
 
-function add(pokemon) {
-  pokemonList.push(pokemon);
-}
+    function add(pokemon) {
+      pokemonList.push(pokemon);
+    }
 
-return {
-  getAll: getAll,
-  add: add,
-  addListItem: addListItem
-};
+    return {
+      getAll: getAll,
+      add: add,
+    };
+
 })();
 
 pokemonRepository.getAll().forEach(function (pokemon) {
-  pokemonRepository.addListItem(pokemon)
+  pokemonRepository.forEach(pokemon)
 });
