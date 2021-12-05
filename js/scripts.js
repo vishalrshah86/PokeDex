1adba3333198c999d91dfb1c8a2b321bd70bdbdc
vishalrshah <vishalrshah86@gmail.com>
2021-12-04 20:09:32 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -44,6 +44,7 @@ let pokemonRepository = (function() {
             detailsUrl: item.url
           };
           add(pokemon);
+          console.log(pokemon);
         });
       }).catch(function (e) {
         console.error(e);
