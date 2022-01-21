b481cf0c9705a3d1ff43962ecfb192908cea570c
vishalrshah <vishalrshah86@gmail.com>
2022-01-20 19:52:49 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -81,76 +81,82 @@ let pokemonRepository = (function () {
   }
 
   /*function showModal(pokemon) {
-    let modalContainer = document.querySelector('#modal-container');
+  let modalContainer = document.querySelector('#modal-container');
 
-    modalContainer.innerHTML = '';
+  modalContainer.innerHTML = '';
 
-    let modal = document.createElement('div');
-    modal.classList.add('modal');
+  let modal = document.createElement('div');
+  modal.classList.add('modal');
 
-    let closeButtonElement = document.createElement('button');
-    closeButtonElement.classList.add('modal-close');
-    closeButtonElement.innerText = 'Close';
-    closeButtonElement.addEventListener('click', hideModal);
+  let closeButtonElement = document.createElement('button');
+  closeButtonElement.classList.add('modal-close');
+  closeButtonElement.innerText = 'Close';
+  closeButtonElement.addEventListener('click', hideModal);
 
-    let pokemonName = document.createElement('h1');
-    pokemonName.innerText = pokemon.name;
+  let pokemonName = document.createElement('h1');
+  pokemonName.innerText = pokemon.name;
 
-    let pokemonType = document.createElement('p');
-    pokemonType.innerText = 'Type(s): ' + pokemon.types.map((x) => x.type.name).join(', ');
+  let pokemonType = document.createElement('p');
+  pokemonType.innerText = 'Type(s): ' + pokemon.types.map((x) => x.type.name).join(', ');
 
-    let pokemonHeight = document.createElement('p');
-    pokemonHeight.innerText = 'Height: ' + pokemon.height;
+  let pokemonHeight = document.createElement('p');
+  pokemonHeight.innerText = 'Height: ' + pokemon.height;
 
 
-    let pokemonImage = document.createElement('img');
-    pokemonImage.classList.add('pokemon-modal-image');
-    pokemonImage.src = pokemon.imageUrl;
+  let pokemonImage = document.createElement('img');
+  pokemonImage.classList.add('pokemon-modal-image');
+  pokemonImage.src = pokemon.imageUrl;
 
-    modal.appendChild(closeButtonElement);
-    modal.appendChild(pokemonName);
-    modal.appendChild(pokemonType);
-    modal.appendChild(pokemonHeight);
-    modal.appendChild(pokemonImage);
-    modalContainer.appendChild(modal);
+  modal.appendChild(closeButtonElement);
+  modal.appendChild(pokemonName);
+  modal.appendChild(pokemonType);
+  modal.appendChild(pokemonHeight);
+  modal.appendChild(pokemonImage);
+  modalContainer.appendChild(modal);
 
-    modalContainer.classList.add('is-visible');
-  }
+  modalContainer.classList.add('is-visible');
+}
 
-  function hideModal() {
-    let modalContainer = document.querySelector('#modal-container');
-    modalContainer.classList.remove('is-visible');
-  }
+function hideModal() {
+let modalContainer = document.querySelector('#modal-container');
+modalContainer.classList.remove('is-visible');
+}
 
-  document.querySelector('#show-modal').addEventListener('click', () => {
-    showModal();
-  });
+document.querySelector('#show-modal').addEventListener('click', () => {
+showModal();
+});
 
-  window.addEventListener('keydown', (e) => {
-    let modalContainer = document.querySelector('#modal-container');
-    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
-      hideModal();
-    }
-  });
+window.addEventListener('keydown', (e) => {
+let modalContainer = document.querySelector('#modal-container');
+if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+hideModal();
+}
+});
 
-  modalContainer.addEventListener('click', (e) => {
-    let target = e.target;
-    if (target === modalContainer) {
-      hideModal();
-    }
-  });
+modalContainer.addEventListener('click', (e) => {
+let target = e.target;
+if (target === modalContainer) {
+hideModal();
+}
+});
 */
 
-  return {
-    add: add,
-    getAll: getAll,
-    addListItem: addListItem,
-    loadList: loadList,
-    loadDetails: loadDetails,
-    showDetails: showDetails,
-    showModal: showModal,
-    hideModal: hideModal,
-  };
+$('[data-toggle="modal"]').on('click', function(){
+  let targetSelector = $(this).attr('data-target');
+  $(targetSelector).modal('show'); // Bootstrap’s own function to make the modal appear
+});
+
+
+return {
+  add: add,
+  getAll: getAll,
+  addListItem: addListItem,
+  loadList: loadList,
+  loadDetails: loadDetails,
+  showDetails: showDetails,
+  showModal: showModal,
+  hideModal: hideModal,
+};
 })();
 
 
