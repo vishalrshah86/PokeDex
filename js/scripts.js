d25f0032941e4c6f0a804e9b2e6166202784d3a4
vishalrshah <vishalrshah86@gmail.com>
2022-01-30 08:35:36 -0500

    removed unwanted codes

--- js/scripts.js
+++ js/scripts.js
@@ -96,67 +96,7 @@ let pokemonRepository = (function () {
     });
   });
 
-  // modal in vanilaJS
-  /*function showModal(pokemon) {
-  let modalContainer = document.querySelector('#modal-container');
-
-  modalContainer.innerHTML = '';
-
-  let modal = document.createElement('div');
-  modal.classList.add('modal');
-
-  let closeButtonElement = document.createElement('button');
-  closeButtonElement.classList.add('modal-close');
-  closeButtonElement.innerText = 'Close';
-  closeButtonElement.addEventListener('click', hideModal);
-
-  let pokemonName = document.createElement('h1');
-  pokemonName.innerText = pokemon.name;
-
-  let pokemonType = document.createElement('p');
-  pokemonType.innerText = 'Type(s): ' + pokemon.types.map((x) => x.type.name).join(', ');
-
-  let pokemonHeight = document.createElement('p');
-  pokemonHeight.innerText = 'Height: ' + pokemon.height;
-
-
-  let pokemonImage = document.createElement('img');
-  pokemonImage.classList.add('pokemon-modal-image');
-  pokemonImage.src = pokemon.imageUrl;
-
-  modal.appendChild(closeButtonElement);
-  modal.appendChild(pokemonName);
-  modal.appendChild(pokemonType);
-  modal.appendChild(pokemonHeight);
-  modal.appendChild(pokemonImage);
-  modalContainer.appendChild(modal);
-
-  modalContainer.classList.add('is-visible');
-}
-
-function hideModal() {
-let modalContainer = document.querySelector('#modal-container');
-modalContainer.classList.remove('is-visible');
-}
-
-document.querySelector('#show-modal').addEventListener('click', () => {
-showModal();
-});
-
-window.addEventListener('keydown', (e) => {
-let modalContainer = document.querySelector('#modal-container');
-if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
-hideModal();
-}
-});
-
-modalContainer.addEventListener('click', (e) => {
-let target = e.target;
-if (target === modalContainer) {
-hideModal();
-}
-});
-*/
+  
 
 function loadList() {
   return fetch(apiUrl).then(function (response) {
